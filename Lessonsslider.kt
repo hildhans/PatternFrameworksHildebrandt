f5c2d2412cc7356d63659efcb94f122a4a40a863
JulietteBraun <43281970+JulietteBraun@users.noreply.github.com>
2023-06-17 23:32:10 +0200

    Add files via upload

---
+++ Lessonsslider.kt
@@ -0,0 +1,66 @@
+package com.example
+
+import io.kvision.core.*
+import io.kvision.html.Tag
+import io.kvision.html.button
+import io.kvision.html.div
+import io.kvision.panel.hPanel
+import io.kvision.toolbar.buttonGroup
+import io.kvision.toolbar.toolbar
+import io.kvision.utils.perc
+
+
+fun Container.lessonsSlider(value: String): Tag {
+    return div(value).apply {
+
+        hPanel(wrap = FlexWrap.WRAP, spacing = 100) {
+            toolbar {
+                buttonGroup {
+                    button("1") {
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                    }
+                }
+                buttonGroup {
+                    button("2", disabled = true){
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                    }
+                    button("3"){
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                    }
+                    button("4"){
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                    }
+                    button("5"){
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                    }
+                    button("6"){
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                    }
+                    button("7"){
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                    }
+                    button("8"){
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                    }
+                    button("9"){
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                    }
+                    button("10"){
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                    }
+                }
+            }
+
+        }
+    }
+}
