ae231e54dd4c5d88b59658986212c357e27e5dc0
JulietteBraun <43281970+JulietteBraun@users.noreply.github.com>
2023-07-07 02:14:36 +0200

    Update PoodlePanel.kt

--- PoodlePanel.kt
+++ PoodlePanel.kt
@@ -1,37 +1,26 @@
-package com.example
+package com.p_f
 
-
+import io.kvision.core.Container
+import io.kvision.html.div
 import io.kvision.i18n.I18n
 import io.kvision.navbar.nav
 import io.kvision.navbar.navLink
 import io.kvision.navbar.navbar
 import io.kvision.panel.SimplePanel
 import io.kvision.panel.vPanel
+import io.kvision.state.bind
 import io.kvision.utils.px
-
-
+import kotlinx.browser.document
 
 object PoodlePanel : SimplePanel(){
     init {
         this.marginTop = 0.px
         this.minHeight = 400.px
         vPanel(spacing = 5) {
-            navbar("", collapseOnClick = true) {
-                nav {
-                    menuBar()
-                }
-                nav(rightAlign = true) {
-                    navLink(I18n.tr(""), icon = "fas fa-home fa-2xl")
-                    navLink(I18n.tr(""), icon = "fas fa-bell fa-2xl")
-                    navLink(I18n.tr(""), icon = "fas fa-power-off fa-2xl")
-                }
-            }
-            mainPart()
+            add(MainMenu)
+            add(MainPart)
         }
     }
-
-
-
 }
 
 
