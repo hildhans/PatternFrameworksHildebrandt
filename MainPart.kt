01ea3409cb325883469e4b1ae388c331c0766aed
JulietteBraun <43281970+JulietteBraun@users.noreply.github.com>
2023-07-07 02:08:51 +0200

    Update MainPart.kt

--- MainPart.kt
+++ MainPart.kt
@@ -1,22 +1,21 @@
-package com.example
+package com.p_f
 
 import io.kvision.core.*
 import io.kvision.html.*
 import io.kvision.i18n.I18n
-import io.kvision.panel.flexPanel
+import io.kvision.modal.Alert
+import io.kvision.panel.FlexPanel
 import io.kvision.utils.px
 
-fun Container.mainPart() {
-
-    flexPanel(
-        FlexDirection.ROW, FlexWrap.WRAP, JustifyContent.FLEXSTART, AlignItems.CENTER,
-        spacing = 5
-    ) {
+object MainPart : FlexPanel( FlexDirection.ROW, FlexWrap.WRAP, JustifyContent.FLEXSTART, AlignItems.CENTER,
+        spacing = 5 ) {
+    init{
         add(sideBar(""), order = 1)
         add(taskBackground(""), order = 2)
+        /*add(lessonsSlider(""), order = 2)*/
+
     }
 }
-
 
 fun Container.sideBar(value: String): Tag {
     return div(value).apply {
@@ -26,30 +25,48 @@ fun Container.sideBar(value: String): Tag {
         height = 600.px
         marginTop = 60.px
         marginLeft = 0.px
+        onClick{
+            Alert.show( I18n.tr("Placeholder"),
+                animation = false )
+        }
 
 
 
-        button(I18n.tr("Übersicht"), style = ButtonStyle.LIGHT) {
+        button(I18n.tr("Overview"), style = ButtonStyle.LIGHT) {
             width = 250.px
             marginTop = 50.px
             fontSize = 15.px
             fontFamily = "Arial"
             marginBottom = 50.px
+            onClick{
+                Alert.show( I18n.tr("Placeholder"),
+                    animation = false )
+            }
         }
 
-        button(I18n.tr("Lektionen"), style = ButtonStyle.INFO) {
+
+
+        button(I18n.tr("Lessons"), style = ButtonStyle.INFO) {
             width = 250.px
             marginTop = 20.px
             fontSize = 15.px
             fontFamily = "Arial"
             marginBottom = 50.px
+            onClick{
+                Alert.show( I18n.tr("Placeholder"),
+                    animation = false )
+            }
         }
 
-        button(I18n.tr("Einstellungen"), style = ButtonStyle.LIGHT) {
+        button(I18n.tr("Configuration"), style = ButtonStyle.LIGHT) {
             width = 250.px
             marginTop = 20.px
             fontSize = 15.px
             fontFamily = "Arial"
+            onClick{
+                Alert.show( I18n.tr("Placeholder"),
+                    animation = false )
+            }
         }
 
     }
