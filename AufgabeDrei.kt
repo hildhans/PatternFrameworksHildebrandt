ff9eadfd73f0ce55c85e38b5f7cb82f1f171e220
JulietteBraun <43281970+JulietteBraun@users.noreply.github.com>
2023-07-07 02:21:28 +0200

    Aufgaben mit teilweise integrierten Funktionen
    Funktionen sind nicht ausgearbeitet. Aufgabe 1 ist z.b. ein Schreibprogramm

---
+++ AufgabeDrei.kt
@@ -0,0 +1,77 @@
+package com.p_f
+
+import io.kvision.core.FlexWrap
+import io.kvision.core.FontWeight
+
+import io.kvision.form.check.CheckStyle
+import io.kvision.form.check.checkBox
+
+import io.kvision.html.ButtonStyle
+
+import io.kvision.html.button
+
+import io.kvision.i18n.I18n.tr
+import io.kvision.i18n.I18n.trans
+import io.kvision.panel.HPanel
+
+import io.kvision.panel.vPanel
+import io.kvision.state.ObservableValue
+import io.kvision.state.bind
+import io.kvision.utils.px
+import io.kvision.core.Container
+
+
+class AufgabeDrei : DesktopWindow("Aufgabe 3 Lesson 1", "", 1000, 800) {
+    init {
+        this.marginTop = 10.px
+        this.minHeight = 400.px
+
+
+            val panel = vPanel(spacing = 5, useWrappers = true)
+
+            data class DataModel(val checked: Boolean, val text: String)
+            data class DataState(val list: List<DataModel>, var searchFilter: String?)
+
+            val dataState = ObservableValue(
+                DataState(
+                    listOf(
+                        DataModel(false, tr("January")),
+                        DataModel(false, tr("February"))
+                    ), null
+                )
+            )
+
+            val dataContainer = HPanel(spacing = 10, wrap = FlexWrap.WRAP).bind(dataState) { state ->
+                state.list.filter { model ->
+                    state.searchFilter?.let {
+                        trans(model.text).contains(it, ignoreCase = true)
+                    } ?: true
+                }.forEach { model ->
+                    checkBox(
+                        value = model.checked,
+                        label = model.text
+                    ) {
+                        flabel.fontWeight = if (model.checked) FontWeight.BOLD else null
+                        style = CheckStyle.PRIMARY
+                        onClick {
+                            val idx = state.list.indexOf(model)
+                            dataState.value = dataState.value.copy(
+                                list = state.list.mapIndexed { index, dataModel -> if (index == idx) dataModel.copy(checked = this.value) else dataModel }
+                            )
+                        }
+                    }
+                }
+            }
+            panel.add(dataContainer)
+
+                button(tr("Remove checked"), style = ButtonStyle.DANGER).onClick {
+                    dataState.value = dataState.value.copy(list = dataState.value.list.filter { !it.checked })
+                }
+            }
+    companion object {
+        fun run(container: Container) {
+            container.add(AufgabeDrei())
+        }
+    }
+}
+
---
+++ AufgabeEins.kt
@@ -0,0 +1,42 @@
+package com.p_f
+
+
+import io.kvision.core.Container
+import io.kvision.core.CssSize
+import io.kvision.core.UNIT
+import io.kvision.form.text.TextInput
+import io.kvision.utils.px
+
+class AufgabeEins : DesktopWindow("Aufgabe 1 Lesson 1", "", 1000, 800) {
+
+    override var height: CssSize?
+        get() = super.height
+        set(value) {
+            super.height = value
+            if (value?.second == UNIT.px) {
+                richText.height = (value.first.toInt() - 93).px
+            }
+        }
+
+    val richText = TextInput()
+
+    init {
+        minWidth = 500.px
+        minHeight = 180.px
+        padding = 3.px
+        richText.height = 370.px
+        add(richText)
+        height = 457.px
+    }
+
+    override fun focus() {
+        super.focus()
+        richText.focus()
+    }
+
+    companion object {
+        fun run(container: Container) {
+            container.add(AufgabeEins())
+        }
+    }
+}
