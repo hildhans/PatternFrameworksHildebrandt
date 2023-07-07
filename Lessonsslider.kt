c1e6f7f5588f78544912a3703d60ea9d7d084121
JulietteBraun <43281970+JulietteBraun@users.noreply.github.com>
2023-07-07 02:10:40 +0200

    Update Lessonsslider.kt

--- Lessonsslider.kt
+++ Lessonsslider.kt
@@ -1,66 +1,108 @@
-package com.example
+package com.p_f
+
 
 import io.kvision.core.*
 import io.kvision.html.Tag
 import io.kvision.html.button
 import io.kvision.html.div
-import io.kvision.panel.hPanel
+import io.kvision.panel.HPanel
 import io.kvision.toolbar.buttonGroup
 import io.kvision.toolbar.toolbar
 import io.kvision.utils.perc
 
+object Lessonsslider : HPanel(wrap = FlexWrap.WRAP, spacing = 100 ) {
+    init{
+      /*  add(lessonsSlider(""))*/
+    }
+}
 
 fun Container.lessonsSlider(value: String): Tag {
     return div(value).apply {
+        marginTop = 4.perc
 
-        hPanel(wrap = FlexWrap.WRAP, spacing = 100) {
+        div {
             toolbar {
                 buttonGroup {
                     button("1") {
                         marginLeft = 4.perc
-                        background = Background(Color.name(Col.ROSYBROWN))
-                    }
-                }
-                buttonGroup {
-                    button("2", disabled = true){
-                        marginLeft = 4.perc
-                        background = Background(Color.name(Col.ROSYBROWN))
-                    }
-                    button("3"){
-                        marginLeft = 4.perc
-                        background = Background(Color.name(Col.ROSYBROWN))
-                    }
-                    button("4"){
-                        marginLeft = 4.perc
-                        background = Background(Color.name(Col.ROSYBROWN))
-                    }
-                    button("5"){
-                        marginLeft = 4.perc
-                        background = Background(Color.name(Col.ROSYBROWN))
-                    }
-                    button("6"){
-                        marginLeft = 4.perc
-                        background = Background(Color.name(Col.ROSYBROWN))
-                    }
-                    button("7"){
-                        marginLeft = 4.perc
-                        background = Background(Color.name(Col.ROSYBROWN))
-                    }
-                    button("8"){
-                        marginLeft = 4.perc
-                        background = Background(Color.name(Col.ROSYBROWN))
-                    }
-                    button("9"){
-                        marginLeft = 4.perc
-                        background = Background(Color.name(Col.ROSYBROWN))
-                    }
-                    button("10"){
-                        marginLeft = 4.perc
-                        background = Background(Color.name(Col.ROSYBROWN))
-                    }
-                }
-            }
 
+                        background = Background(Color.name(Col.ROSYBROWN))
+                        onClick {
+                            AufgabeEins.run(this@lessonsSlider)
+                        }
+                    }
+                    button("2") {
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                        onClick {
+                            AufgabeZwei.run(this@lessonsSlider)
+                        }
+                    }
+                    button("3") {
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                        onClick {
+                            AufgabeDrei.run(this@lessonsSlider)
+                        }
+                    }
+                    button("4") {
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                        onClick {
+                            AufgabeVier.run(this@lessonsSlider)
+                        }
+                    }
+                    button("5") {
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                        onClick {
+                            AufgabeFuenf.run(this@lessonsSlider)
+                        }
+                    }
+                    button("6") {
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                        onClick {
+                            AufgabeSechs.run(this@lessonsSlider)
+                        }
+                    }
+                    button("7") {
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                        onClick {
+                            AufgabeSieben.run(this@lessonsSlider)
+                        }
+                    }
+                    button("8") {
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                        onClick {
+                            AufgabeAcht.run(this@lessonsSlider)
+                        }
+                    }
+                    button("9") {
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                        onClick {
+                            AufgabeNeun.run(this@lessonsSlider)
+                        }
+                    }
+                    button("10") {
+                        marginLeft = 4.perc
+                        background = Background(Color.name(Col.ROSYBROWN))
+                        onClick {
+                            AufgabeZehn.run(this@lessonsSlider)
+                        }
+                    }
+
+
+                }
+
+
+            }
         }
+
     }
+
+
 }
