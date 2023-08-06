8f0ecedef8f26aabc6e4297d88944e926e7da723
JulietteBraun <43281970+JulietteBraun@users.noreply.github.com>
2023-08-07 00:28:48 +0200

    Erste Aufgabe angelegt

--- AufgabeDrei.kt
+++ AufgabeDrei.kt
@@ -1,24 +1,21 @@
 package com.p_f
 
+import io.kvision.core.Container
 import io.kvision.core.FlexWrap
 import io.kvision.core.FontWeight
-
 import io.kvision.form.check.CheckStyle
 import io.kvision.form.check.checkBox
-
 import io.kvision.html.ButtonStyle
-
 import io.kvision.html.button
-
+import io.kvision.i18n.I18n
 import io.kvision.i18n.I18n.tr
 import io.kvision.i18n.I18n.trans
+import io.kvision.modal.Alert
 import io.kvision.panel.HPanel
-
 import io.kvision.panel.vPanel
 import io.kvision.state.ObservableValue
 import io.kvision.state.bind
 import io.kvision.utils.px
-import io.kvision.core.Container
 
 
 class AufgabeDrei : DesktopWindow("Aufgabe 3 Lesson 1", "", 1000, 800) {
@@ -26,20 +23,29 @@ class AufgabeDrei : DesktopWindow("Aufgabe 3 Lesson 1", "", 1000, 800) {
         this.marginTop = 10.px
         this.minHeight = 400.px
 
-
             val panel = vPanel(spacing = 5, useWrappers = true)
 
-            data class DataModel(val checked: Boolean, val text: String)
+            data class DataModel(val hallo: Boolean, val text: String)
+        data class DataModel2(val tschuess: Boolean, val text: String)
             data class DataState(val list: List<DataModel>, var searchFilter: String?)
+        data class DataState2(val list: List<DataModel2>, var searchFilter:String?)
 
             val dataState = ObservableValue(
                 DataState(
                     listOf(
-                        DataModel(false, tr("January")),
-                        DataModel(false, tr("February"))
-                    ), null
+                        DataModel(false, tr("Hello"))
+                    ), null,
                 )
             )
+
+        val dataState2 = ObservableValue(
+            DataState2(
+                listOf(
+                    DataModel2(false, tr("Good bye"))
+                ), null,
+            )
+        )
+
 
             val dataContainer = HPanel(spacing = 10, wrap = FlexWrap.WRAP).bind(dataState) { state ->
                 state.list.filter { model ->
@@ -48,15 +54,15 @@ class AufgabeDrei : DesktopWindow("Aufgabe 3 Lesson 1", "", 1000, 800) {
                     } ?: true
                 }.forEach { model ->
                     checkBox(
-                        value = model.checked,
+                        value = model.hallo,
                         label = model.text
                     ) {
-                        flabel.fontWeight = if (model.checked) FontWeight.BOLD else null
+                        flabel.fontWeight = if (model.hallo) FontWeight.BOLD else null
                         style = CheckStyle.PRIMARY
                         onClick {
                             val idx = state.list.indexOf(model)
                             dataState.value = dataState.value.copy(
-                                list = state.list.mapIndexed { index, dataModel -> if (index == idx) dataModel.copy(checked = this.value) else dataModel }
+                                list = state.list.mapIndexed { index, dataModel -> if (index == idx) dataModel.copy(hallo = this.value) else dataModel }
                             )
                         }
                     }
@@ -64,9 +70,46 @@ class AufgabeDrei : DesktopWindow("Aufgabe 3 Lesson 1", "", 1000, 800) {
             }
             panel.add(dataContainer)
 
-                button(tr("Remove checked"), style = ButtonStyle.DANGER).onClick {
-                    dataState.value = dataState.value.copy(list = dataState.value.list.filter { !it.checked })
+
+        val dataContainer2 = HPanel(spacing = 10, wrap = FlexWrap.WRAP).bind(dataState2) { state ->
+            state.list.filter { model ->
+                state.searchFilter?.let {
+                    trans(model.text).contains(it, ignoreCase = true)
+                } ?: true
+            }.forEach { model ->
+                checkBox(
+                    value = model.tschuess,
+                    label = model.text
+                ) {
+                    flabel.fontWeight = if (model.tschuess) FontWeight.BOLD else null
+                    style = CheckStyle.PRIMARY
+                    onClick {
+                        val idx = state.list.indexOf(model)
+                        dataState2.value = dataState2.value.copy(
+                            list = state.list.mapIndexed { index, dataModel2 -> if (index == idx) dataModel2.copy(tschuess = this.value) else dataModel2}
+                        )
+                    }
                 }
+            }
+        }
+        panel.add(dataContainer2)
+
+                button(tr("Hallo"), style = ButtonStyle.DANGER).onClick {
+                    dataState.value = dataState.value.copy(list = dataState.value.list.filter { !it.hallo })
+                }
+
+                button(tr("Tschüss"), style = ButtonStyle.DANGER).onClick {
+                    dataState2.value = dataState2.value.copy(list = dataState2.value.list.filter { !it.tschuess})
+                }
+
+
+
+                 button(I18n.tr("Aufgabenstellung"), style = ButtonStyle.SECONDARY).onClick {
+                     Alert.show(
+                        I18n.tr("Aufgabenstellung: Lektion 1, Lesson 10"),
+                        "Fügen Sie die richtigen Wortpaare zusammen.",
+                         animation = false )
+                        }
             }
     companion object {
         fun run(container: Container) {
