61b4166cfc4d0410238eb61a761bfff1b75e8de3
JulietteBraun <43281970+JulietteBraun@users.noreply.github.com>
2023-08-14 01:42:25 +0200

    Update AufgabeFuenf.kt
    Versuch eine Flashcard-Aufgabe zu bauen

--- AufgabeFuenf.kt
+++ AufgabeFuenf.kt
@@ -5,37 +5,69 @@ import io.kvision.core.CssSize
 import io.kvision.core.UNIT
 import io.kvision.form.text.TextInput
 import io.kvision.utils.px
+import io.kvision.core.onClick
+import io.kvision.form.form
+import io.kvision.form.text.textAreaInput
+import io.kvision.html.ButtonStyle
+import io.kvision.html.button
+import io.kvision.html.div
+import io.kvision.i18n.I18n
+import io.kvision.modal.Alert
+import io.kvision.panel.gridPanel
+import io.kvision.panel.vPanel
+import io.kvision.panel.vPanel
+
+data class Flashcard(val question: String, val answer: String)
 
 class AufgabeFuenf : DesktopWindow("Aufgabe 5 Lesson 1", "", 1000, 800) {
 
-    override var height: CssSize?
-        get() = super.height
-        set(value) {
-            super.height = value
-            if (value?.second == UNIT.px) {
-                richText.height = (value.first.toInt() - 93).px
+    private val flashcards = mutableListOf(
+        Flashcard("We like to dance", "Yes, we like it very much"),
+        Flashcard("Do you have a dog", "Yes, his name ist Benno"),
+        Flashcard("I traveled to Budapest, when", "I was a studend"),
+        Flashcard("When I am at home", "I usually relax."),
+    )
+
+    private var currentFlashcardIndex = 0
+
+
+
+        init {
+            button("Next Flashcard").onClick {
+                currentFlashcardIndex = (currentFlashcardIndex + 1) % flashcards.size
+                flashcards[currentFlashcardIndex].question
             }
-        }
+            div {
+                button("Frage: ").onClick {
+                    +flashcards[currentFlashcardIndex].question
+                }
+            }
+            div {
+                button("Antwort: ").onClick {
+                    +flashcards[currentFlashcardIndex].answer
+                }
 
-    val richText = TextInput()
+            }
 
-    init {
-        minWidth = 500.px
-        minHeight = 180.px
-        padding = 3.px
-        richText.height = 370.px
-        add(richText)
-        height = 457.px
-    }
+            button(I18n.tr("Aufgabenstellung"), style = ButtonStyle.SECONDARY).onClick {
+                Alert.show(
+                    I18n.tr("Aufgabenstellung: Lektion 1, Lesson 5"),
+                    "Merke dir, welche Fagen zu welchen Antworten gehören.",
+                    animation = false )
+            }
 
-    override fun focus() {
-        super.focus()
-        richText.focus()
-    }
 
-    companion object {
-        fun run(container: Container) {
-            container.add(AufgabeFuenf())
-        }
+
+            }
+
+
+companion object {
+    fun run(container: Container) {
+        container.add(AufgabeFuenf())
     }
 }
+}
+
+
+
+
